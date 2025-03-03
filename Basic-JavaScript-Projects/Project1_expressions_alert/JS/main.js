d5d36a25096e898e80f54438509f1c07b8a52782
manishmukund91 <manishmukund91@gmail.com>
2025-03-02 21:19:40 -0500

    New javascript project first commit

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+// Display an alert message when the page loads
+window.alert("Welcome to my JavaScript project!");
+
+// Declare two string variables
+var Sent1 = "This is the beginning of the string"; // First part of the sentence
+var Sent2 = " and this is the end of the string"; // Second part of the sentence
+
+// Concatenate the two strings and display them on the webpage
+document.write(Sent1 + Sent2);
+
+// Write an expression and display the result
+document.write("<br>The sum of 10 + 5 is: " + (10 + 5)); // Expression calculation
